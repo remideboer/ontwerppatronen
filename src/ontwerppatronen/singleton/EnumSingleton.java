34d260d2086ffb1c824f4184b73d1518ea232355
Remi de Boer <remideboer@yahoo.com>
2017-03-07 17:23:03 +0100

    singleton uitgebreid met runner en Enum singleton

---
+++ src/ontwerppatronen/singleton/EnumSingleton.java
@@ -0,0 +1,12 @@
+package ontwerppatronen.singleton;
+
+/**
+ *
+ * @author Remi
+ */
+public enum EnumSingleton {
+    INSTANCE;
+    public void String(){
+        
+    }
+}
---
+++ src/ontwerppatronen/singleton/SingletonRunner.java
@@ -0,0 +1,11 @@
+package ontwerppatronen.singleton;
+
+/**
+ *
+ * @author Remi
+ */
+public class SingletonRunner {
+    public static void main(String[] args) {
+        Singleton s = Singleton.getInstance();
+    }
+}
